3aa03ca74ec6ba0a9626956f1cf8bc117fe6e7df
Jakob Holderbaum <webmaster@techfolio.de>
2009-06-11 15:20:32 +0000

    just for merging

--- cms/cms.rb
+++ cms/cms.rb
@@ -8,15 +8,24 @@ module CMS
     require 'cms/Core/Block'
     require 'cms/Config/Config'
 
+    require 'cms/Userspace/Block'
 
-    block = Core::Block.new('name','da title','atype')
-    block.blocksrc="just a test\n maan"
+#    block = Core::Block.new('name','da title','atype')
+#    block.blocksrc="just a test\n maan"
 #    block.load
-    block.dump
+#    block.dump
 
     cfg = Config::Configuration.new 'config.yaml',nil
 
     puts cfg.system.path['html']
+
+    puts Userspace::MKBLOCK_DESCR
+   
+    string = 'mkblock'
+
+    var = eval('Userspace::'+string.upcase+'_DESCR')
+
+    puts var
 
 
 end
