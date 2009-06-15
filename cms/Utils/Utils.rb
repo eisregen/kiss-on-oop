04e0d9951931a8090bf9ee58c0b2191137d866ce
Nils <mail@n-sch.de>
2009-06-15 22:12:53 +0200

    foo

--- cms/Utils/Utils.rb
+++ cms/Utils/Utils.rb
@@ -1,4 +1,3 @@
-module CMS
 #
 # bunch of wellused utilities
 #
@@ -6,25 +5,47 @@ module CMS
 #   Contact:    privat (at) techfolio.de | irc: #kis on xinutec.org
 #
 
-
 require 'rubygems'
 require 'RedCloth'
 
-module Utils # {{{
+module CMS
+
+  module Utils
+
+    # Get the tree structure as an array of Strings
+    def Utils.get_tree (parent, struct = Structure::Structure.new.load)
+
+      result = []
+      struct.get_children(parent).each do |child|
+
+        result <<= child
+        if struct.has_children? child
+          result.concat(get_tree(child).map {|s| " |-" + s})
+        end
+
+      end
+
+      return result
+    end
+
+    # Turn an Array of Strings into one String, separated by "\n"
+    def Utils.unlines (array)
+      array[0..-2].map{|x| x << "\n"} << array[-1]
+    end
 
     # checks the given argument against the defined validation rule
     # because of the savings as file, the names of blocks and pages shoud only
     # contain alphanumeric characters
     def Utils.valid? (name) # {{{
-        name =~ /^\w+$/
+      name =~ /^\w+$/
     end # }}}
 
     # just a little wrapperfunction around the RedCloth textile onject.  it does
     # not really improve the speed or something like that, it is just for
     # readability
     def Utils.textile (string) # {{{
-        RedCloth.new(IO.readlines(blkfile).join).to_html
+      RedCloth.new(IO.readlines(blkfile).join).to_html
     end # }}}
 
-end # }}}
+  end # }}}
 end
